42567b70b20ddaf99b956063e2fe7f36c764b220
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-07-02 02:19:31 +0600

    Loading and manage if found no movies

--- Http-Requests/src/App.js
+++ Http-Requests/src/App.js
@@ -5,9 +5,9 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([])
-
+  const [isLoading,setIsLoading] = useState(false)
   async function  fetchMoviesHandler() {
-
+    setIsLoading(true)
     const response = await fetch('https:/swapi.dev/api/films/')
     const data = await response.json()
 
@@ -21,6 +21,7 @@ function App() {
 
     })
     setMovies(transformedMovies)
+    setIsLoading(false)
   }
   return (
     <React.Fragment>
@@ -28,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>No movies found</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
