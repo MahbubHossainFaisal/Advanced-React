688c9af972cc380fa30006593dc84b8c4750256f
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-07-02 02:09:08 +0600

    Using async await instead of .then

--- Http-Requests/src/App.js
+++ Http-Requests/src/App.js
@@ -1,27 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const [movies,setMovies] = useState([])
+  const [movies, setMovies] = useState([])
 
-  const fetchMoviesHandler = () => {
-    fetch('https:/swapi.dev/api/films/')
-      .then((response) =>{
-        return response.json()
-      }).then((data) =>{
-         const transformedMovies = data.results.map((movieData) =>{
-           return {
-             id: movieData.episode_id,
-             title: movieData.title,
-             openingText: movieData.opening_crawl,
-             releaseDate: movieData.release_date
-           }
-         })
+  async function  fetchMoviesHandler() {
 
-         setMovies(transformedMovies)
-      })
+    const response = await fetch('https:/swapi.dev/api/films/')
+    const data = await response.json()
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+
+    })
+    setMovies(transformedMovies)
   }
   return (
     <React.Fragment>
