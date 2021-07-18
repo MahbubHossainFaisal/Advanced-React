3a8e06db96c27e351bf662e97c4da59f1751a763
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-07-18 21:01:32 +0600

    Using redux in react components

--- React-Redux/src/index.js
+++ React-Redux/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import store from './store/reduxStore'
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}>
+    <App />
+</Provider>,
+    document.getElementById('root'));
