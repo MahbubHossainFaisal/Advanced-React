2b664b8726558a34466ba5753501cdc8f2a93942
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-07-21 00:22:41 +0600

    Reading and dispatching items using authSlice

--- React-Redux/src/App.js
+++ React-Redux/src/App.js
@@ -1,15 +1,18 @@
+
+import React from 'react';
 import Counter from './components/Counter';
 import Header from './components/Header'
 import Auth from './components/Auth'
 import UserProfile from './components/UserProfile'
-import React from 'react';
+import {useSelector} from 'react-redux'
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated)
   return (
    <React.Fragment>
       <Header />
-      <Auth />
-      <UserProfile />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
    </React.Fragment>
   );
