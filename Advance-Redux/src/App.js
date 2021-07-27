a3ec7c8a7c1abb5bd1123d8f0a457ac09e7237d4
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-07-27 23:55:20 +0600

    Adding fetch request using useEffect

--- Advance-Redux/src/App.js
+++ Advance-Redux/src/App.js
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -7,6 +7,15 @@ import Products from './components/Shop/Products';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+
+  useEffect(()=>{
+    fetch('https://redux-http-2ebc0-default-rtdb.firebaseio.com/cart.json',{
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    })
+  },[cart])
+
   return (
     <Layout>
       {showCart && <Cart />}
