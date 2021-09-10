e5c6febdad9f846114cab08e620c30d4751fc770
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-11 02:23:46 +0600

    NavLinks

--- React-Router/src/components/Header.js
+++ React-Router/src/components/Header.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import classes from './Header.module.css'
 const Header = () => {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to='/welcome'>Welcome Page</Link>
-                </li>
-                <li>
-                    <Link to='/products'>Products Page</Link>
-                </li>
-            </ul>
-        </div>
+        <header className={classes.header}>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink activeClassName={classes.active} to='/welcome'>Welcome</NavLink>
+                    </li>
+                    <li>
+                        <NavLink activeClassName={classes.active} to='/products'>Products</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
