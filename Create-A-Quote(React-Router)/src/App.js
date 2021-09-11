1ee0d248829ea520c14bc650f97105ea754a1784
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-12 01:43:07 +0600

    Create A Quote project -Routes initialization

---
+++ Create-A-Quote(React-Router)/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetails from './pages/QuoteDetails';
+
+
+function App() {
+  return (
+    <Switch>
+      <Route path='/' exact>
+        <Redirect to='/quotes' />
+      </Route>
+      <Route path='/quotes' exact> 
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuoteDetails />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
---
+++ Create-A-Quote(React-Router)/src/pages/QuoteDetails.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuoteDetails = () => {
+    return (
+        <div>
+           <h1>Quote details page</h1> 
+        </div>
+    )
+}
+
+export default QuoteDetails
