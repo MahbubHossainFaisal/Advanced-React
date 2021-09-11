ec7ae10ba18ab40b137fe52cf226348c26892396
Mahbub Hossain Faisal <mahbubhossain249@gmail.com>
2021-09-12 02:05:56 +0600

    Redirecting and nested route in QuoteDetails page

--- Create-A-Quote(React-Router)/src/pages/QuoteDetails.js
+++ Create-A-Quote(React-Router)/src/pages/QuoteDetails.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useParams,Route } from 'react-router-dom'
+import Comments from '../components/comments/Comments'
 
 const QuoteDetails = () => {
+    const params = useParams()
     return (
-        <div>
-           <h1>Quote details page</h1> 
-        </div>
+        <React.Fragment>
+           <h1>Quote details page</h1>
+           <p>{params.quoteId}</p>
+           <Route path={`/quotes/${params.quoteId}/comments`}>
+              <Comments />
+           </Route>
+        </React.Fragment>
     )
 }
 
